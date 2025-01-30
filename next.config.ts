85a8baaba42cfba01396c62c0d20efca315c7773
helloayan14 <ayansharma2006@gmail.com>
2025-01-30 20:02:00 +0530

    lasting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone", // Remove "export" if present
+// Remove "export" if present
 
 };
 
