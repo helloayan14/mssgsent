3f6c3aa55a5c358eb5339c1a08e1a8f10151ea39
helloayan14 <ayansharma2006@gmail.com>
2025-01-30 20:29:29 +0530

    should

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 // Remove "export" if present
-
+   output: "standalone",
 };
 
 export default nextConfig;
